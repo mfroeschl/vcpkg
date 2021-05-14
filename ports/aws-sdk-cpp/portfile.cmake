dc2ee6ad5e48add5b3dbb7c29b81f2eadfce8235
Marcus Froeschl <marcus@ageet.com>
2021-05-14 13:25:11 +0900

    Add ClientConfig patch
    Fixes delay due to unnecessary region detection in constructor of ClientConfig

--- ports/aws-sdk-cpp/portfile.cmake
+++ ports/aws-sdk-cpp/portfile.cmake
@@ -9,6 +9,7 @@ vcpkg_from_github(
     PATCHES
         patch-relocatable-rpath.patch
         fix-AWSSDKCONFIG.patch
+		fix-ClientConfig.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "dynamic" FORCE_SHARED_CRT)
